3bf0d3df46241afd3833245ff0178600ea50f01d
Tcherno So <tcherno.so@hotmail.com>
2023-12-02 12:19:20 +0000

    Update data-structures.js

--- BasicJS/data-structures.js
+++ BasicJS/data-structures.js
@@ -77,10 +77,84 @@ console.log(months);
 //slice() takes only 2 parameters — the first is the index at which to begin extraction, and the second is the index at which to stop extraction (extraction will occur up to, but not including the element at this index).
 
 function forecast(arr) {
-    // Only change code below this line
   
     return arr.slice(2,4);
   }
   
-  // Only change code above this line
   console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms'])); //returns [ 'warm', 'sunny' ]
+
+
+
+
+
+
+// Copy Array Items Using ES6's new spread operator allows us to easily copy all of an array's elements, in order, with a simple and highly readable syntax. The spread syntax simply looks like this: ...
+
+let thisArray = [true, true, undefined, false, null];
+let thatArray = [...thisArray];
+
+
+
+// ex: 
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+   newArr.push([...arr]);
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
+
+
+
+// Combine Arrays with ES6 spread operator 
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander']
+
+
+// ex: 
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence = ['learning', ...fragment, 'is','fun'] ; 
+  return sentence;
+}
+
+console.log(spreadOut()); //returns ["learning", "to", "code", "is", "fun"]
+
+
+
+
+// Check For The Presence of an Element With indexOf() can be incredibly useful for quickly checking for the presence of an element on an array. 
+
+let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+
+fruits.indexOf('dates'); // -1 when the a element doesnt exist
+fruits.indexOf('oranges'); // 2
+fruits.indexOf('pears'); // 1
+
+
+// ex: 
+
+function quickCheck(arr, elem) {
+  if (arr.indexOf(elem) >= 0) {
+    return true;
+  }
+  return false;
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+// with less code 
+function quickCheck(arr, elem) {
+  return arr.indexOf(elem) >= 0 ? true : false;
+}
+console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
+
+// or 
+function quickCheck(arr, elem) {
+  return arr.indexOf(elem) != -1;
+}
+console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
